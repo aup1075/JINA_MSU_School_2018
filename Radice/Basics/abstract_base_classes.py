59a60887b2bfc551c7c6c61651d0d85b8082529e
Prakash <aup1075@E1-054967.local>
2022-06-05 20:20:01 -0400

    Basics: - Modified a few comments

--- Radice/Basics/abstract_base_classes.py
+++ Radice/Basics/abstract_base_classes.py
@@ -1,19 +1,21 @@
 """
-An example implementation of abstract base classes in Python ~ Templates in C++
+An example of abstract base classes in Python 
 Please refer tutorial at:
 https://github.com/CodeSports/Modern-Python-Programming/blob/master/PythonABC/dakshhub_python_abc.py
 """
 
 import abc 
 
-# An abstract base class is a template of (child) classes to come. 
+# An abstract base class is a parent class of (child) classes to come. 
+
+# class providing a skeleton/interface/boiler-plate of State.
 class State(abc.ABC):
 	
 	@abc.abstractmethod
 	def in_state(self):
 		pass					
 
-# Classes implementing the template 		
+# Classes implementing the abstract base class of state. 		
 class HappyState(State):
 	def in_state(self):
 		print("I am in a happy state")
@@ -24,11 +26,13 @@ class SadState(State):
 		
 #### ---------------------------------- ####
 
+# class providing a skeleton/interface/boiler-plate of Event. 
 class Event(abc.ABC):
 	@abc.abstractmethod
 	def in_event(self):
 		pass
 
+# Classes providing an implementation of the abc Event. 
 class recieved_money(Event):
 	def in_event(self):
 		print("I got money")
